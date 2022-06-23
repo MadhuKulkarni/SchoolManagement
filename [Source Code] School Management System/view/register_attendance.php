266880fa994fc7439200a3dafd782e2fda7c3438
minu88 <minususanj@gmail.com>
2022-06-23 19:18:44 +0530

    added try and error

--- [Source Code] School Management System/view/register_attendance.php
+++ [Source Code] School Management System/view/register_attendance.php
@@ -7,27 +7,39 @@
     // echo "</pre>";
     // exit;
     
-    foreach($dataVal as $val) {
-        $studId = $val['studId'];
-        $subject = $val['subject'];
-        $date = $val['date'];
-        $time = $val['time'];
-        $attendance = $val['attendance'];
-
-      
-        $sql = "INSERT INTO my_attendance (studId, subject, date, time, attendance) VALUES( ".$studId.", '" . $subject . "', '" . $date . "', '" . $time . "', '" . $attendance . "')";
+try{
+        foreach($dataVal as $val) {
+            $studId = $val['studId'];
+            $subject = $val['subject'];
+            $date = $val['date'];
+            $time = $val['time'];
+            $attendance = $val['attendance'];
 
         
+            $sql = "INSERT INTO my_attendance (studId, subject, date, time, attendance) VALUES( ".$studId.", '" . $subject . "', '" . $date . "', '" . $time . "', '" . $attendance . "')";
+
+            
+            
+          
+            $var_msg = "This is an exception example";
+            throw new Exception($var_msg);
+            if(!mysqli_query($conn, $sql)) {
+                // $response = 'Error inserting the duplicates';
+                $response = 'Error inserting the duplicates';
+                throw new Exception($var_msg);
+            }
         
-        if(!mysqli_query($conn, $sql)) {
-            $response = 'Error...please try again later';
         }
-    
+
+        $response = 'Attendance registered successfully';
     }
    
-    $response = 'Attendance registered successfully';
+    catch(Exception $e){
+        echo $response = $e->getMessage();
+    }
+   
 
-    echo $response;
+    // echo $response;
 ?>
 
 
